302a693c67f94fa81980e5089f42ab391ab0f928
lsp_zone <15712860161@163.com>
2019-07-23 11:36:14 +0800

    map遍历

--- src/Test/test1.java
+++ src/Test/test1.java
@@ -1,5 +1,20 @@
 package Test;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+
 public class test1 {
+	public static void main(String[] args) {
+		HashMap<String,Integer> map = new HashMap<String,Integer>();
+		map.put("李四", 20);
+		map.put("王五", 23);
+		map.put("张三",30);
+		
+		Set<String> Set = map.keySet();
+		for (String key : Set) {
+			System.out.println(key+""+map.get(key));
+		}
+	}
 
 }
